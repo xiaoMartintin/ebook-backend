5bddcc7a87a120423e998c6e62a395bc869c37d6
xiaoMartintin <>
2024-06-29 03:09:57 +0800

    完结撒花！！！！！

--- src/main/java/com/klx/ebookbackend/controller/CartController.java
+++ src/main/java/com/klx/ebookbackend/controller/CartController.java
@@ -1,6 +1,8 @@
 package com.klx.ebookbackend.controller;
 
+import com.klx.ebookbackend.entity.Book;
 import com.klx.ebookbackend.entity.Cart;
+import com.klx.ebookbackend.service.BookService;
 import com.klx.ebookbackend.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,16 +16,19 @@ import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
 
     private final CartService cartService;
+    private final BookService bookService;
 
     @Autowired
-    public CartController(CartService cartService) {
+    public CartController(CartService cartService, BookService bookService) {
         this.cartService = cartService;
+        this.bookService = bookService;
     }
 
     @GetMapping()
@@ -46,6 +51,12 @@ public class CartController {
         if (userId == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createResponse("Unauthorized", false, null));
         }
+
+        Book book = bookService.getBookById(bookId);
+        if (book == null || book.getInventory() < 1) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createResponse("Not enough inventory", false, null));
+        }
+
         int quantity = 1; // 默认数量为 1
         cartService.addCartItem(userId, bookId, quantity);
         return ResponseEntity.ok(createResponse("Item added to cart", true, null));
@@ -57,6 +68,18 @@ public class CartController {
         if (userId == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createResponse("Unauthorized", false, null));
         }
+
+        Optional<Cart> cartItemOptional = cartService.getCartById(id);
+        if (!cartItemOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createResponse("Cart item not found", false, null));
+        }
+
+        Cart cartItem = cartItemOptional.get();
+        Book book = bookService.getBookById(cartItem.getBook().getId());
+        if (book == null || book.getInventory() < quantity) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createResponse("Not enough inventory", false, null));
+        }
+
         cartService.changeCartItemQuantity(id, quantity);
         return ResponseEntity.ok(createResponse("Cart item quantity updated", true, null));
     }
